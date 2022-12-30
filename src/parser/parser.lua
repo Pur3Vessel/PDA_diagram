aafe8089b2fa21b7f22e8f0b231f0da21af747ad
Pur3Vessel <pur3vessel@mail.ru>
2022-12-30 15:24:17 +0300

    small refactor

--- src/parser/parser.lua
+++ src/parser/parser.lua
@@ -43,56 +43,56 @@ function Parser:initialize(config)
             self.al_sym = value
             for _,  elem in ipairs(self.prohibited) do
                 if string.match(elem, value) ~= nil then
-                    error('Токен параметризирован не уникальным значением')
+                    error(string.format("Токен %s параметризирован не уникальным значением", token))
                 end
             end
         elseif token == '[stack-symbol]' then
             self.stack_symbol = value
             for _,  elem in ipairs(self.prohibited) do
                 if string.match(elem, value) ~= nil then
-                    error('Токен параметризирован не уникальным значением')
+                    error(string.format("Токен %s параметризирован не уникальным значением", token))
                 end
             end
         elseif token == '[flag]' then
             self.flag = value
             self.prohibited['flag'] = nil
             if has_value(self.prohibited, value) or string.match(value, self.al_sym) ~= nil or string.match(value, self.stack_symbol) ~= nil then
-                error("Токен параметризирован не уникальным значением")
+                error(string.format("Токен %s параметризирован не уникальным значением", token))
             end 
             self.prohibited['flag'] = value
         elseif token == '[sep]' then
             self.sep = value
             self.prohibited['sep'] = nil
             if has_value(self.prohibited, value) or string.match(value, self.al_sym) ~= nil or string.match(value, self.stack_symbol) ~= nil then
-                error('Токен параметризирован не уникальным значением')
+                error(string.format("Токен %s параметризирован не уникальным значением", token))
             end 
             self.prohibited['sep'] = value
         elseif token == '[trans-sep]' then
             self.trans_sep = value
             self.prohibited['trans-sep'] = nil
             if has_value(self.prohibited, value) or string.match(value, self.al_sym) ~= nil or string.match(value, self.stack_symbol) ~= nil then
-                error('Токен параметризирован не уникальным значением')
+                error(string.format("Токен %s параметризирован не уникальным значением", token))
             end 
             self.prohibited['trans-sep'] = value
         elseif token == '[empty]' then
             self.empty = value
             self.prohibited['empty'] = nil
             if has_value(self.prohibited, value) or string.match(value, self.al_sym) ~= nil or string.match(value, self.stack_symbol) ~= nil then
-                error('Токен параметризирован не уникthальным значением')
+                error(string.format("Токен %s параметризирован не уникальным значением", token))
             end 
             self.prohibited['empty'] = value
         elseif token == '[stack-any]' then
             self.any = value
             self.prohibited['any'] = nil
             if has_value(self.prohibited, value) or string.match(value, self.al_sym) ~= nil or string.match(value, self.stack_symbol) ~= nil then
-                error('Токен параметризирован не уникальным значением')
+                error(string.format("Токен %s параметризирован не уникальным значением", token))
             end 
             self.prohibited['stack-any'] = value
         elseif token == '[stack-bottom]' then
             self.bottom = value
             self.prohibited['bottom'] = nil
             if has_value(self.prohibited, value) or string.match(value, self.al_sym) ~= nil or string.match(value, self.stack_symbol) ~= nil then
-                error('Токен параметризирован не уникальным значением')
+                error(string.format("Токен %s параметризирован не уникальным значением", token))
             end 
             self.prohibited['bottom'] = value
         else
@@ -118,14 +118,14 @@ function Parser:parseAutomata(filename)
     for i, name in ipairs(states_names) do
         local name_sep = split(name, "-")
         if #name_sep == 0 or #name_sep > 2 then
-            error("Некорретная запись состояния")
+            error(string.format("Некорретная запись состояния %s", name))
         end
         if has_value(self.prohibited, name_sep[1]) then
-            error("Запрещенное имя состояния")
+            error(string.format("Запрещенное имя состояния %s", name))
         end
         for char in string.gmatch(name_sep[1], ".") do
             if has_value(self.prohibited, char) then
-                error("Запрещенное имя состояния")
+                error(string.format("Запрещенное имя состояния %s", name))
             end
         end
         table.insert(self.states, name_sep[1])
@@ -134,7 +134,7 @@ function Parser:parseAutomata(filename)
         end
         if #name_sep == 2 then
             if name_sep[2] ~= self.flag then
-                error("Некорретный маркер завершающего состояния")
+                error(string.format("Некорретный маркер завершающего состояния: %s", name_sep[2]))
             end
             print('Это состояние завершающее', name_sep[1])
         end
@@ -144,60 +144,67 @@ function Parser:parseAutomata(filename)
         local transition = lines[i]
         local transition_separated = split(transition, self.trans_sep)
         if #transition_separated ~= 2 then
-            error('Некорректная запись перехода')
+            error(string.format('Некорректная запись перехода %s', transition))
         end
         local before = transition_separated[1]
         local after = transition_separated[2]
         local before_separated = split(before, ',')
         local after_separated = split(after, ',')
         if #before_separated ~= 3 or #after_separated ~= 2 then
-            error('Некорректная запись перехода')
+            error(string.format('Некорректная запись перехода %s', transition))
         end
         if #before_separated[1] == 0 or #before_separated[2] == 0 or #before_separated[3] == 0 or  #after_separated[1] == 0 or #after_separated[2] == 0 then
-            error('Некорректная запись перехода')
+            error(string.format('Некорректная запись перехода %s', transition))
         end
         if not has_value(self.states, before_separated[1]) or not has_value(self.states, after_separated[1]) then
-            error('В переходе появилось необъявленное состояние')
+            error(string.format('В переходе появилось необъявленное состояние %s', transition))
         end
         if string.match(before_separated[2],self.al_sym) == nil and before_separated[2] ~= self.empty then
-            error('В переходе появился символ, не принадлежащий входному алфавиту')
+            error(string.format('В переходе появился символ, не принадлежащий входному алфавиту %s', transition))
         end
         if string.match(before_separated[3], self.stack_symbol) == nil and before_separated[3] ~= self.bottom and before_separated[3] ~= self.any then
-            error('В переходе появился символ, не принадлежащий стэковому алфавиту')
+            error(string.format('В переходе появился символ, не принадлежащий стэковому алфавиту %s', transition))
+        end
+        if #before_separated[2] ~= 1 and before_separated[2] ~= self.empty or #before_separated[3] ~= 1 and before_separated[3] ~= self.any then
+            error(string.format('Некорректная запись перехода %s', transition))
         end
         if #after_separated[2] == 1 then
             if after_separated[2] == self.any and before_separated[3] ~= self.any then
-                error('Справа появился любой стэковый символ, а слева нет')
+                error(string.format('Справа появился любой стэковый символ, а слева нет %s', transition))
             end
             if after_separated[2] ~= self.bottom and before_separated[3] == self.bottom then
-                error('Было считано дно стэка, но его не вернули')
+                error(string.format('Было считано дно стэка, но его не вернули %s', transition))
             end
             if after_separated[2] == self.bottom and before_separated[3] ~= self.bottom then
-                error('Дно стэка не было считано, но оно было снова записано')
+                error(string.format('Дно стэка не было считано, но оно было снова записано %s', transition))
             end
-            if string.match(after_separated[2], self.stack_symbol) == nil and after_separated[2] ~= self.empty and after_separated[2] ~= self.bottom then
-                error('В переходе появился символ, не принадлежащий стэковому алфавиту')
+            if string.match(after_separated[2], self.stack_symbol) == nil and after_separated[2] ~= self.empty and after_separated[2] ~= self.bottom and after_separated[2] ~= self.any then
+                error(string.format('В переходе появился символ, не принадлежащий стэковому алфавиту %s', transition))
             end
         else
             if after_separated[2] ~= self.empty then
                 local before_last = string.sub(after_separated[2], 1, #after_separated[2] - 1)
                 local last = string.sub(after_separated[2], -1)
                 for char in string.gmatch(before_last, ".") do
-                    if string.match(char, self.stack_symbol) == nil or char == self.bottom then
-                        error('В переходе появился символ, не принадлежащий стэковому алфавиту')
+                    if string.match(char, self.stack_symbol) == nil then
+                        error(string.format('В переходе появился символ, не принадлежащий стэковому алфавиту %s', transition))
                     end
                 end
                 if before_separated[3] == self.bottom and last ~= self.bottom then
-                    error('Было считано дно стэка, но его не вернули')
+                    error(string.format('Было считано дно стэка, но его не вернули %s', transition))
                 end
                 if before_separated[3] ~= self.bottom then
                     if string.match(last, self.stack_symbol) == nil then
-                        error('В переходе появился символ, не принадлежащий стэковому алфавиту')
+                        error(string.format('В переходе появился символ, не принадлежащий стэковому алфавиту %s', transition))
                     end
                 end
-            else
+            elseif after_separated[2] ~= self.any then
                 if before_separated[3] == self.bottom then
-                    error('Было считано дно стэка, но его не вернули')
+                    error(string.format('Было считано дно стэка, но его не вернули %s', transition))
+                end
+            else
+                if before_separated[3] ~= self.any then
+                    error(string.format('Справа появился любой стэковый символ, а слева нет %s', transition))
                 end
             end
         end
